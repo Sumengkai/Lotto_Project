9883ca48a3ffe74715376e78c9c43b88d7dbc416
Sumengkai <a0973038822@gmail.com>
2024-05-13 10:53:14 +0800

    1_20240513

---
+++ Backend/Lotto_Project/src/main/java/com/example/Lotto_Project/LottoProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.Lotto_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LottoProjectApplication.class, args);
+	}
+
+}
---
+++ Backend/Lotto_Project/src/test/java/com/example/Lotto_Project/LottoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lotto_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
